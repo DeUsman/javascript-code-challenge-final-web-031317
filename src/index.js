1b1f9fcd6b39b8edeaa4199d8fef07794b1d0bf5
Usman Pervaiz <de.usman@yahoo.com>
2017-05-12 10:38:19 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
   // your code here!
 })
+
+$("input[type='submit']").click(function(e){
+	e.preventDefault()
+	var userInput = $("input[name='image_url']")
+	var userCaption = $("input[name='caption']")
+
+	$("#photo-list").append(function(){
+		console.log(userInput.val())
+		return `<div><figure><img src=${userInput.val()} alt=${userCaption.val()} style='width:300px;height:300px' alt="${userCaption}"/><figcaption style="color:red">${userCaption.val()}</figcaption></figure></div><br>`
+	})
+	userInput.val("")
+	userCaption.val("")
+	
+})
+
+
+
